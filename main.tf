504bc1651f9590c58d8ba2da1bddfecb4a4e1569
rpsjenkins <rohanshetty81@gmail.com>
2019-12-17 13:55:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,13 @@ terraform {
 
 resource "azurerm_resource_group" "main" {
   name     = "RPSResourceGroup"
-  location = "${var.location}"
+  location = "var.location"
   }
 
 resource "azurerm_app_service_plan" "main" {
-  name                = "${var.prefix}-asp"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
+  name                = "var.prefix-asp"
+  location            = "azurerm_resource_group.main.location"
+  resource_group_name = "azurerm_resource_group.main.name"
   kind                = "Linux"
   reserved            = true
 
@@ -25,10 +25,10 @@ resource "azurerm_app_service_plan" "main" {
 }
 
 resource "azurerm_app_service" "main" {
-  name                = "${var.prefix}-appservice"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+  name                = "var.prefix-appservice"
+  location            = "azurerm_resource_group.main.location"
+  resource_group_name = "azurerm_resource_group.main.name"
+  app_service_plan_id = "azurerm_app_service_plan.main.id"
 
   ## site_config {
   ##   dotnet_framework_version = "v4.0"
