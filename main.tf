df3c00bd69d133da8b42af8af15f224c75a14d0b
rpsjenkins <rohanshetty81@gmail.com>
2019-12-17 11:05:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,9 +22,9 @@ resource "azurerm_app_service" "main" {
   resource_group_name = "${azurerm_resource_group.main.name}"
   app_service_plan_id = "${azurerm_app_service_plan.main.id}"
 
-  site_config {
-    dotnet_framework_version = "v4.0"
-    remote_debugging_enabled = true
-    remote_debugging_version = "VS2015"
-  }
+  ## site_config {
+  ##   dotnet_framework_version = "v4.0"
+  ##   remote_debugging_enabled = true
+  ##   remote_debugging_version = "VS2015"
+ ##  }
 }
